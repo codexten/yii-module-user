5103392302aa4a9dc3fd8606b730af4a54c2899c
Jomon Johnson <jomon.entero@gmail.com>
2019-07-02 10:42:14 +0530

    init

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -2,9 +2,11 @@
 
 namespace codexten\yii\modules\user\controllers;
 
+use codexten\yii\modules\auth\models\User;
 use codexten\yii\web\CrudController;
 
-abstract class UserController extends CrudController
+class UserController extends CrudController
 {
+    public $modelClass = User::class;
 
 }
